084e180f6e92d6ea322f220bd246d8e202339bad
ntasicc <nikols.tt@elfak.rs>
2021-12-08 19:12:12 +0100

    Error fixed

--- DrawBoard.py
+++ DrawBoard.py
@@ -82,10 +82,14 @@ def ValidatePawnMove(pawnsPosition: dict, player: str, pawn: int, newSpot: tuple
     oldY = oldSpot[1] * 2 - 1
 
     # Proverava da li je pomeranje ucinjeno za dva mesta ili za jedno mesto ka cilju
-    goal = pawnsPosition["start" + 'O' if player == 'X' else 'X']
-    distance = abs(oldSpot[1]-newSpot[0])+abs(oldSpot[0]-newSpot[1])
-    if(distance == 1 and goal[0] != newSpot and goal[1] != newSpot):
-        if(distance != 2):
+    goal = pawnsPosition['start' + ('O' if player == 'X' else 'X')]
+    print(goal)
+    distance = abs(oldSpot[0]-newSpot[0])+abs(oldSpot[1]-newSpot[1])
+    print(distance)
+    if(distance != 2):
+        if(distance == 1 and goal[0] != newSpot and goal[1] != newSpot):
+            return False
+        elif(distance != 1):
             return False
 
     # Proverava da li je nova pozicija van granica table
@@ -96,25 +100,25 @@ def ValidatePawnMove(pawnsPosition: dict, player: str, pawn: int, newSpot: tuple
 
     # Gore
     if(newSpot[0] < oldSpot[0] and newSpot[1] == oldSpot[1]):
-        if((tabla[newX+1][newY] == '===') if distance == 2 else False or tabla[newX+3][newY] == '==='):
+        if(((tabla[newX+1][newY] == '===') if distance == 2 else False) or (tabla[newX+3][newY] == '===')):
             return False
         elif (((tabla[newX][newY] == "X") or (tabla[newX][newY] == "O")) and (tabla[newX+2][newY] == " ")):
-            return (newSpot[0]-1, newSpot[1])
+            return (newSpot[0]+1, newSpot[1])
         else:
             return newSpot
 
     # Dole
     elif(newSpot[0] > oldSpot[0] and newSpot[1] == oldSpot[1]):
-        if((tabla[newX-1][newY] == '===') if distance == 2 else False or tabla[newX-3][newY] == '==='):
+        if(((tabla[newX-1][newY] == '===') if distance == 2 else False) or (tabla[newX-3][newY] == '===')):
             return False
         elif (((tabla[newX][newY] == "X") or (tabla[newX][newY] == "O")) and (tabla[newX-2][newY] == " ")):
-            return (newSpot[0]+1, newSpot[1])
+            return (newSpot[0]-1, newSpot[1])
         else:
             return newSpot
 
     # Levo
     elif(newSpot[0] == oldSpot[0] and newSpot[1] < oldSpot[1]):
-        if((tabla[newX][newY+1] == ' ||') if distance == 2 else False or tabla[newX][newY+3] == ' ||'):
+        if(((tabla[newX][newY+1] == ' ||') if distance == 2 else False) or (tabla[newX][newY+3] == ' ||')):
             return False
         elif (((tabla[newX][newY] == "X") or (tabla[newX][newY] == "O")) and (tabla[newX][newY+2] == " ")):
             return (newSpot[0], newSpot[1]+1)
@@ -123,7 +127,7 @@ def ValidatePawnMove(pawnsPosition: dict, player: str, pawn: int, newSpot: tuple
 
     # Desno
     elif(newSpot[0] == oldSpot[0] and newSpot[1] > oldSpot[1]):
-        if((tabla[newX][newY-1] == ' ||') if distance == 2 else False or tabla[newX][newY-3] == ' ||'):
+        if(((tabla[newX][newY-1] == ' ||') if distance == 2 else False) or (tabla[newX][newY-3] == ' ||')):
             return False
         elif (((tabla[newX][newY] == "X") or (tabla[newX][newY] == "O")) and (tabla[newX][newY-2] == " ")):
             return (newSpot[0], newSpot[1]-1)
@@ -166,18 +170,12 @@ def ValidatePawnMove(pawnsPosition: dict, player: str, pawn: int, newSpot: tuple
     return newSpot
 
 
-'''
-initialStateOfPawns(pawnsDict, (1, 1),
-                    (2, 1), (4, 41), (5, 4))
-print(ValidatePawnMove(pawnsDict, 'X', 1, (1, 3)))'''
-
-
 def StartingBoard(tableSizeN: int, tableSizeM: int):
     # Tabla je matrica koja ce da se stampa, sadrzi igrace, prazna polja i zidove
-    newTableSizeN=tableSizeN * 2 + 1
-    newTableSizeM=tableSizeM * 2 + 1
+    newTableSizeN = tableSizeN * 2 + 1
+    newTableSizeM = tableSizeM * 2 + 1
     for i in range(0, newTableSizeN):
-        red=list()
+        red = list()
         if (i % 2 == 0):
             for j in range(0, newTableSizeM):
                 if (i == 0 or i == newTableSizeN - 1):
@@ -188,8 +186,8 @@ def StartingBoard(tableSizeN: int, tableSizeM: int):
             for j in range(0, newTableSizeM):
                 red.append(" | " if (j % 2 == 0)
                            else pozicije[(i - 1) // 2][(j - 1) // 2])
-            red[0]="||"
-            red[newTableSizeM - 1]="||"
+            red[0] = "||"
+            red[newTableSizeM - 1] = "||"
         tabla.append(red)
 
 
@@ -197,24 +195,24 @@ def DrawWalls():
     # Dodavanje zidova u tablu koja se iscrtava
     for i in range(0, len(list(wallDict['H']))):
         tabla[(wallDict['H'][i][0] + 1) *
-              2][wallDict['H'][i][1] * 2 + 1]="==="
+              2][wallDict['H'][i][1] * 2 + 1] = "==="
         tabla[(wallDict['H'][i][0] + 1) *
-              2][wallDict['H'][i][1] * 2 + 3]="==="
+              2][wallDict['H'][i][1] * 2 + 3] = "==="
 
     for i in range(0, len(list(wallDict['V']))):
         tabla[wallDict['V'][i][0] * 2 +
-              1][(wallDict['V'][i][1] + 1) * 2]=" ||"
+              1][(wallDict['V'][i][1] + 1) * 2] = " ||"
         tabla[wallDict['V'][i][0] * 2 +
-              3][(wallDict['V'][i][1] + 1) * 2]=" ||"
+              3][(wallDict['V'][i][1] + 1) * 2] = " ||"
 
 
 def AddWall(color: str, position: tuple):
     if color == 'p':
-        tabla[position[0] * 2][position[1] * 2 - 1]="==="
-        tabla[position[0] * 2][position[1] * 2 + 1]="==="
+        tabla[position[0] * 2][position[1] * 2 - 1] = "==="
+        tabla[position[0] * 2][position[1] * 2 + 1] = "==="
     else:
-        tabla[position[0] * 2 - 1][position[1] * 2]=" ||"
-        tabla[position[0] * 2 + 1][position[1] * 2]=" ||"
+        tabla[position[0] * 2 - 1][position[1] * 2] = " ||"
+        tabla[position[0] * 2 + 1][position[1] * 2] = " ||"
 
 
 def DrawTable():
