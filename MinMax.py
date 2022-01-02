e377853cec53a274811ee6ddd0e8a9d8978bbfe5
ntasicc <nikols.tt@elfak.rs>
2022-01-02 16:06:36 +0100

    .

--- MinMax.py
+++ MinMax.py
@@ -13,11 +13,13 @@ def max_value(tabla, depth, alpha, beta, pawnsDict, wallDict, tableSizeN, tableS
             tabla, pawnsDict, wallDict, "X", 1, tableSizeN, tableSizeN)
         list_states.append(allValidStates(
             tabla, pawnsDict, wallDict, "X", 2, tableSizeN, tableSizeN))
-        for s in list_states:
+        for s in list_states[0]:
+            index = list_states[0].index(s)
             alpha = max(alpha, min_value(tabla, depth-1, alpha, beta,
-                                         pawnsDict, wallDict, tableSizeN, tableSizeM), key=lambda x: x[1])
+                                         list_states[1][index], list_states[2][index], tableSizeN, tableSizeM), key=lambda x: x[1])
             if alpha[1] >= beta[1]:
                 return beta
+
     return alpha
 
 
@@ -29,9 +31,10 @@ def min_value(tabla, depth, alpha, beta, pawnsDict, wallDict, tableSizeN, tableS
             tabla, pawnsDict, wallDict, "O", 1, tableSizeN, tableSizeN)
         list_states.append(allValidStates(
             tabla, pawnsDict, wallDict, "O", 2, tableSizeN, tableSizeN))
-        for s in list_states:
+        for s in list_states[0]:
+            index = list_states[0].index(s)
             beta = max(beta, max_value(tabla, depth-1, alpha, beta,
-                                       pawnsDict, wallDict, tableSizeN, tableSizeM), key=lambda x: x[1])
+                                       list_states[1][index], list_states[2][index], tableSizeN, tableSizeM), key=lambda x: x[1])
             if beta[1] <= alpha[1]:
                 return alpha
     return beta
