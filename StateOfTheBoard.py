de8ee0cabeb730c155fe1a01feaa1e5c83a15f6a
ntasicc <nikols.tt@elfak.rs>
2022-01-02 16:06:22 +0100

    allValidState sada vraca i stanja pijuna i zidova za svaki validan potez

--- StateOfTheBoard.py
+++ StateOfTheBoard.py
@@ -1,6 +1,6 @@
 from Pawns import movePawn
 from DrawBoard import DrawMove, ValidatePawnMove, DrawPawnMove
-from Walls import validWall, numOfWalls
+from Walls import placeWall, validWall, numOfWalls
 from Pathfinding import astar
 import copy
 
@@ -18,6 +18,8 @@ def changeState(tabla: list, pawnsPositions: dict, color: str, position: tuple, 
 
 def allValidStates(tabla: list, pawnsDict: dict, wallDict: dict, player: str, pawn: int, tableSizeN: int, tableSizeM: int):
     validTables = list()
+    validPD = list()
+    validWD = list()
     validSpots = list()
     coord: tuple = pawnsDict[player][pawn-1]
     spotsToCheck: list = [(coord[0]+2, coord[1]), (coord[0]-2, coord[1]), (coord[0], coord[1]+2), (coord[0], coord[1]-2),
@@ -29,6 +31,7 @@ def allValidStates(tabla: list, pawnsDict: dict, wallDict: dict, player: str, pa
             validSpots.append(spotAfterValidation)
 
     oldPawnsDict = copy.deepcopy(pawnsDict)
+    oldWallDict = copy.deepcopy(wallDict)
 
     boje = ["p", "z"]
     for i in range(0, 2):
@@ -42,14 +45,23 @@ def allValidStates(tabla: list, pawnsDict: dict, wallDict: dict, player: str, pa
                                                    (i, j), player, pawn, v, True)
                             movePawn(oldPawnsDict, player, pawn,
                                      v)
+                            placeWall(oldWallDict, player, bojaZida, (i, j))
 
                             if(astar(newTable, oldPawnsDict['X'][0], oldPawnsDict['startO'][0]) and astar(newTable, oldPawnsDict['X'][0], oldPawnsDict['startO'][1]) and astar(newTable, oldPawnsDict['X'][1], oldPawnsDict['startO'][0]) and astar(newTable, oldPawnsDict['X'][1], oldPawnsDict['startO'][1]) and astar(newTable, oldPawnsDict['O'][0], oldPawnsDict['startX'][0]) and astar(newTable, oldPawnsDict['O'][0], oldPawnsDict['startX'][1]) and astar(newTable, oldPawnsDict['O'][1], oldPawnsDict['startX'][0]) and astar(newTable, oldPawnsDict['O'][1], oldPawnsDict['startX'][1])):
                                 validTables.append(newTable)
+                                validPD.append(oldPawnsDict)
+                                validWD.append(oldWallDict)
 
                             oldPawnsDict = copy.deepcopy(pawnsDict)
+                            oldWallDict = copy.deepcopy(wallDict)
         else:
             for v in validSpots:
                 newTable = changeState(tabla, oldPawnsDict, "",
                                        (0, 0), player, pawn, v, False)
+                movePawn(oldPawnsDict, player, pawn,
+                         v)
+                validPD.append(oldPawnsDict)
                 validTables.append(newTable)
-    return validTables
+                validWD.append(oldWallDict)
+                oldPawnsDict = copy.deepcopy(pawnsDict)
+    return [validTables, validPD, validWD]
