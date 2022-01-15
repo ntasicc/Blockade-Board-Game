acd9af7fc32ceb010503d933371891dd3e301ee6
Danilo-Mladenovic <danilo.mladenovic999@gmail.com>
2022-01-15 21:42:27 +0100

    minimax3 radi i kad ne dodje do dubine 3

--- MinMax.py
+++ MinMax.py
@@ -57,37 +57,6 @@ def checkValue(stanje):
     return score
 
 
-def max_stanje(lsv, alpha, beta):
-    alpha = max(max(lsv, key=lambda x: x[1]), alpha, key=lambda x: x[1])
-    if alpha[1] >= beta[1]:
-        return beta
-    return alpha
-
-
-def min_stanje(lsv, alpha, beta):
-    beta = min(min(lsv, key=lambda x: x[1]), beta, key=lambda x: x[1])
-    if beta[1] <= alpha[1]:
-        return alpha
-    return beta
-
-
-def minimax2(stanje, dubina, moj_potez, tableSizeN, tableSizeM, alpha, beta, potez=None):
-    igrac = "X" if moj_potez else "O"
-    fja = max_stanje if moj_potez else min_stanje
-    lp = allValidStates(
-        stanje[0], stanje[1], stanje[2], igrac, 1, tableSizeN, tableSizeM)
-    tempList = allValidStates(
-        stanje[0], stanje[1], stanje[2], igrac, 2, tableSizeN, tableSizeM)
-    lp.extend(tempList)
-    if dubina == 0:
-        return (potez, checkValue(stanje))
-
-    if lp is None or len(lp) == 0:
-        return (potez, checkValue(stanje))
-
-    return fja(([minimax2(x, dubina - 1, not moj_potez, tableSizeN, tableSizeM, alpha, beta, x if potez is None else potez) for x in lp]), alpha, beta)
-
-
 def minimax3(state, depth, maximizingPlayer, tableSizeN, tableSizeM, alpha, beta, potez=None):
 
     if depth == 3:
@@ -98,6 +67,9 @@ def minimax3(state, depth, maximizingPlayer, tableSizeN, tableSizeM, alpha, beta
         state[0], state[1], state[2], igrac, 1, tableSizeN, tableSizeM)
     all_states.extend(allValidStates(
         state[0], state[1], state[2], igrac, 2, tableSizeN, tableSizeM))
+
+    if all_states is None or len(all_states) == 0:
+        return (potez, checkValue(state))
 
     if maximizingPlayer:
         best = (("banana"), -1000)
